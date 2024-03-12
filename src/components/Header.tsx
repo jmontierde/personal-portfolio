4507851192b7a8e796aab8dc2db6ee07288678e8
Jayson Caburatan <montierdejayson@gmail.com>
2024-03-12 15:17:41 +0800

    build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 const Header = () => {
-  const image1 = "./images/bg1.png"
+  const image1 = "./images/bg1.png";
   return (
-      <div className='flex justify-center items-center h-screen bg-no-repeat bg-center' style={{backgroundImage: `url(${image1})`}}>
-      <h1 className='container px-6 text-3xl text-white md:text-7xl max-w-7xl font-satoshi '>
-        Design solutions <span className='opacity-50'>that are</span>  visually 
-        appealing, impactful, and aligned 
-        <span className='opacity-50'> with your brand's messaging and goals.</span>
-        </h1>
-      </div>
-    
-  )
-}
+    <div
+      className="flex justify-center items-center h-screen bg-no-repeat bg-center"
+      style={{ backgroundImage: `url(${image1})` }}
+    >
+      <h1 className="container px-6 text-3xl text-white md:text-7xl max-w-7xl font-satoshi ">
+        Design solutions <span className="opacity-50">vaping experiences</span>{" "}
+        where quality and satisfaction
+        <span className="opacity-50"> are our top priorities.</span>
+      </h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const Project = () => {
   return (
-    <section id='projects'>
-      <div className='container px-6 mx-auto'>
-          <h2 className='md:mt-48 mt-24 text-xl md:text-5xl text-white max-w-5xl opacity-80'>Producing high-quality designs that effectively convey clients unique vision and goals. </h2>
-          <div className='grid md:grid-cols-2 mt-12 md:mt-32 gap-12 cursor-pointer'>
-              <img className='h-96 w-full opacity-50 hover:opacity-100 ' src="./images/work1.png" alt="" />
-              <img className='h-96 w-full opacity-50 hover:opacity-100' src="./images/work2.png" alt="" />
-              <img className='h-96 w-full opacity-50 hover:opacity-100' src="./images/work3.png" alt="" />
-              <img className='h-96 w-full opacity-50 hover:opacity-100' src="./images/work4.png" alt="" />
-          </div>
+    <section id="projects">
+      <div className="container px-6 mx-auto">
+        <h2 className="md:mt-48 mt-24 text-xl md:text-5xl text-white max-w-5xl opacity-80">
+          Producing high-quality web projects that effectively convey clients'
+          unique vision and goals.
+        </h2>
+        <div className="grid md:grid-cols-2 mt-12 md:mt-32 gap-12 cursor-pointer">
+          <img
+            className="h-96 w-full opacity-50 hover:opacity-100 "
+            src="./images/ecom.png"
+            alt=""
+          />
+          <img
+            className="h-96 w-full opacity-50 hover:opacity-100 "
+            src="./images/work1.png"
+            alt=""
+          />
+          <img
+            className="h-96 w-full opacity-50 hover:opacity-100"
+            src="./images/work2.png"
+            alt=""
+          />
+          <img
+            className="h-96 w-full opacity-50 hover:opacity-100"
+            src="./images/work3.png"
+            alt=""
+          />
+          <img
+            className="h-96 w-full opacity-50 hover:opacity-100"
+            src="./images/work4.png"
+            alt=""
+          />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
